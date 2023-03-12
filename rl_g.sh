092f8c15f850298e77ff5c8a4a1e82e97c369569
odoldotol <odoldotol@gmail.com>
2023-03-12 23:02:26 +0900

    fix: sh

--- rl_g.sh
+++ rl_g.sh
@@ -2,12 +2,20 @@ cd getMarket
 ps u -e | grep uvicorn | grep main | awk '{print $2}' | xargs kill -15
 while :
 do
-    IS_SERVER_LIVE=$(command ps u -e | grep uvicorn | grep main | wc -l)
-    if [ ${IS_SERVER_LIVE} -eq 0 ]
+    if [ $(command ps u -e | grep uvicorn | grep main | wc -l) -eq 0 ]
     then
         break
     fi
     sleep 0.5
 done
+echo "server down"
 nohup uvicorn main:app --workers 3 &
-tail -f -n 50 nohup.out
+while :
+do
+    if [ $(command tail -n 15 nohup.out | grep "Application startup complete." | wc -l) -eq 3 ]
+    then
+        break
+    fi
+    sleep 0.5
+done
+tail -n 12 nohup.out
