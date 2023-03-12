714e7afdd2a13c88f78ef5960a2b0d8621a6a3b6
odoldotol <odoldotol@gmail.com>
2023-03-12 21:50:34 +0900

    fix: sh

--- rl_g.sh
+++ rl_g.sh
@@ -1,6 +1,13 @@
 cd getMarket
 ps u -e | grep uvicorn | grep main | awk '{print $2}' | xargs kill -15
-wait
+while :
+do
+    IS_SERVER_LIVE=$(command ps u -e | grep uvicorn | grep main | wc -l)
+    if [ ${IS_SERVER_LIVE} -eq 0 ]
+    then
+        break
+    fi
+    sleep 0.5
+done
 nohup uvicorn main:app --workers 3 &
-wait
-tail -n 100 nohup.out
+tail -f -n 50 nohup.out
