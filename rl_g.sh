92e196deabf51edf579ccb1f7377259c5ea1a581
odoldotol <odoldotol@gmail.com>
2023-03-12 21:15:56 +0900

    fix: sh

--- rl_g.sh
+++ rl_g.sh
@@ -2,4 +2,5 @@ cd getMarket
 ps u -e | grep uvicorn | grep main | awk '{print $2}' | xargs kill -15
 wait
 nohup uvicorn main:app --workers 3 &
-tail -f -n 100 nohup.out
+wait
+tail -n 100 nohup.out
