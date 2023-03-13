03fd8f140212ed15a67b4244e4069ba12e92afda
odoldotol <odoldotol@gmail.com>
2023-03-14 00:12:14 +0900

    fix: kakao ChatBot temp auth

--- product/src/kakaoCB/kakaoCB.controller.ts
+++ product/src/kakaoCB/kakaoCB.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { KakaoCBService } from './kakaoCB.service';
 import { SkillPayloadDto } from './dto/SkillPayload.dto';
 
 @Controller('kakaoCB')
 export class KakaoCBController {
 
+    private readonly KAKAO_CHATBOT_ID: string = this.configService.get('KAKAO_CHATBOT_ID');
+
     constructor(
+        private readonly configService: ConfigService,
         private readonly kakaoCBService: KakaoCBService,
     ) {}
 
@@ -14,6 +18,9 @@ export class KakaoCBController {
      */
     @Post('inquire')
     inquire(@Body() body/*: SkillPayloadDto*/) {
+        if (body.bot.id !== this.KAKAO_CHATBOT_ID) {
+            throw new UnauthorizedException();
+        };
         // console.log(body);
         return this.kakaoCBService.inquire(body);
     }
