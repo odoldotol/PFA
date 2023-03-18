aedeed738f35ad79b471f6999bfd475f539232d8
odoldotol <odoldotol@gmail.com>
2023-03-19 07:53:15 +0900

    pm2 eco - prod, dev

--- market/ecosystem.config.js
+++ market/ecosystem.config.js
@@ -1,15 +1,43 @@
 module.exports = {
   apps : [
     {
-      name: 'pfa_market',
+      name: 'pfa_market_prod',
       script: 'dist/main.js',
       instances: 1,
       exec_mode: 'cluster',
+      // watch: ['dist'], // restart 하는것때문에 버림
       // wait_ready: true, // 필요없음
       listen_timeout: 600000, // 10분
       kill_timeout: 60000, // 1분
       max_restarts: 50,
       restart_delay: 500,
+      max_memory_restart: '300M',
+
+      env: {
+        NODE_ENV: 'production',
+        PORT: 6000,
+        PM2: 1,
+        PM2_NAME: 'pfa_market_prod',
+      },
+    },
+    {
+      name: 'pfa_market_dev',
+      script: 'dist/main.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      watch: ['dist'],
+      // wait_ready: true, // 필요없음
+      listen_timeout: 600000, // 10분
+      kill_timeout: 60000, // 1분
+      max_restarts: 50,
+      restart_delay: 500,
+      max_memory_restart: '300M',
+
+      env: {
+        NODE_ENV: 'development',
+        PORT: 6000,
+        PM2_NAME: 'pfa_market_dev',
+      },
     },
   ],
 
--- product/ecosystem.config.js
+++ product/ecosystem.config.js
@@ -1,15 +1,42 @@
 module.exports = {
   apps : [
     {
-      name: 'pfa_product',
+      name: 'pfa_product_prod',
       script: 'dist/main.js',
       instances: 1,
       exec_mode: 'cluster',
+      // watch: ['dist'], // restart 하는것때문에 버림
       // wait_ready: true, // 필요없음
       listen_timeout: 60000, // 1분
       kill_timeout: 20000, // 20초
       max_restarts: 50,
       restart_delay: 500,
+      max_memory_restart: '200M',
+
+      env: {
+        NODE_ENV: 'production',
+        PORT: 7000,
+        PM2_NAME: 'pfa_product_prod',
+      },
+    },
+    {
+      name: 'pfa_product_dev',
+      script: 'dist/main.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      watch: ['dist'],
+      // wait_ready: true, // 필요없음
+      listen_timeout: 60000, // 1분
+      kill_timeout: 20000, // 20초
+      max_restarts: 50,
+      restart_delay: 500,
+      max_memory_restart: '200M',
+
+      env: {
+        NODE_ENV: 'development',
+        PORT: 7000,
+        PM2_NAME: 'pfa_product_dev',
+      },
     },
   ],
 
