6ed3aebf18a99518a496e274a630a8c86b359f27
odoldotol <odoldotol@gmail.com>
2023-03-19 20:30:36 +0900

    Fix: Kakao Refer Cache Object

--- product/src/kakaoCB/kakaoCB.service.ts
+++ product/src/kakaoCB/kakaoCB.service.ts
@@ -31,7 +31,7 @@ export class KakaoCBService {
                     },
                 ],
             },
-            data: kakaoData,
+            data: {...kakaoData, kakaoText},
         }
     }
 
