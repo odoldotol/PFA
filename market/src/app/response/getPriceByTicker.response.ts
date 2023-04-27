a677372bdc104eb3070edcca102752305c03bd0a
odoldotol <odoldotol@gmail.com>
2023-04-28 01:32:12 +0900

    Comment: Todo

--- market/src/app/response/getPriceByTicker.response.ts
+++ market/src/app/response/getPriceByTicker.response.ts
@@ -11,7 +11,7 @@ export class ResponseGetPriceByTicker {
     @ApiProperty({type: String, example: 'USD'})
     readonly currency: string;
 
-    // TODO
+    // TODO - 제거되어야함. 상위클래스로 묶어야함. getPriceByTicker 함수 리팩터링 필요.
     // @ApiProperty({required: false})
     readonly status_price?: StatusPrice;
 
