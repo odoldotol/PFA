5840aadd668658a76d17488cd94a98fd65ab139e
odoldotol <odoldotol@gmail.com>
2023-04-28 23:37:07 +0900

    Refac: Either/test - class define

--- market/src/common/class/either.class.spec.ts
+++ market/src/common/class/either.class.spec.ts
@@ -1,8 +1,14 @@
-import { Either } from "./either.class";
+import { Either, EitherRight, EitherLeft } from "./either.class";
 
 describe('Either', () => {
 
-    it('should be defined', () => {
+    it('Either should be defined', () => {
         expect(Either).toBeDefined();});
+    
+    it('EitherRight should be defined', () => {
+        expect(EitherRight).toBeDefined();});
+    
+    it('EitherLeft should be defined', () => {
+        expect(EitherLeft).toBeDefined();});
 
 });
