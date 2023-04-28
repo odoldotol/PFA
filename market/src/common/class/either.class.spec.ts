7fa7f822b333ddde66c433b74680caa5e51d824f
odoldotol <odoldotol@gmail.com>
2023-04-29 02:10:17 +0900

    Refac: Either/Test(Dedup)

--- market/src/common/class/either.class.spec.ts
+++ market/src/common/class/either.class.spec.ts
@@ -11,50 +11,42 @@ describe('Either', () => {
     it('EitherLeft should be defined', () => {
         expect(EitherLeft).toBeDefined();});
     
+    let eitherRight: Either<string, string>
+    let eitherLeft: Either<string, string>
+
+    beforeEach(() => {
+        eitherRight = Either.right('right_value');
+        eitherLeft = Either.left('left_value');});
+    
     describe('creation', () => {
         it('Either.right', () => {
-            const eitherRight = Either.right<string, string>('right_value');
             expect(eitherRight).toBeInstanceOf(EitherRight);
             expect(eitherRight).toBeInstanceOf(Either);
-            expect(eitherRight).not.toBeInstanceOf(EitherLeft);
-        });
+            expect(eitherRight).not.toBeInstanceOf(EitherLeft);});
     
         it('Either.left', () => {
-            const eitherLeft = Either.left<string, string>('left_value');
             expect(eitherLeft).toBeInstanceOf(EitherLeft);
             expect(eitherLeft).toBeInstanceOf(Either);
-            expect(eitherLeft).not.toBeInstanceOf(EitherRight);
-        });
-    });
+            expect(eitherLeft).not.toBeInstanceOf(EitherRight);});});
 
     describe('check either Right or Left', () => {
         it('Right', () => {
-            const eitherRight = Either.right<string, string>('right_value');
             expect(eitherRight.isRight()).toBeTruthy();
-            expect(eitherRight.isLeft()).toBeFalsy();
-        });
+            expect(eitherRight.isLeft()).toBeFalsy();});
     
         it('Left', () => {
-            const eitherLeft = Either.left<string, string>('left_value');
             expect(eitherLeft.isRight()).toBeFalsy();
-            expect(eitherLeft.isLeft()).toBeTruthy();
-        });
-    });
+            expect(eitherLeft.isLeft()).toBeTruthy();});});
 
     describe('get value', () => {
         it('Right', () => {
-            const eitherRight = Either.right<string, string>('right_value');
             expect(eitherRight.getWhatever).toBe('right_value');
             expect(eitherRight.getRight).toBe('right_value');
-            expect(() => eitherRight.getLeft).toThrow();
-        });
-    
+            expect(() => eitherRight.getLeft).toThrow();});
+
         it('Left', () => {
-            const eitherLeft = Either.left<string, string>('left_value');
             expect(eitherLeft.getWhatever).toBe('left_value');
             expect(eitherLeft.getLeft).toBe('left_value');
-            expect(() => eitherLeft.getRight).toThrow();
-        });
-    });
+            expect(() => eitherLeft.getRight).toThrow();});});
 
 });
