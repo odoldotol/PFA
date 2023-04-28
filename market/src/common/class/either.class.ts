fc19b97bf8b0f8f4a9856b0c6c5271929df45b40
odoldotol <odoldotol@gmail.com>
2023-04-28 23:44:13 +0900

    Refac: Either/green - class define

--- market/src/common/class/either.class.ts
+++ market/src/common/class/either.class.ts
@@ -9,8 +9,8 @@ export class Either<L, R> {
         throw new Error('Either must have a value');
     }
   
-    static right = <L, R>(value: R): Either<L, R> => new this(undefined, value) as Either<L, R>;
-    static left = <L, R>(value: L): Either<L, R> => new this(value, undefined) as Either<L, R>;
+    // static right = <L, R>(value: R): Either<L, R> => new this(undefined, value) as Either<L, R>;
+    // static left = <L, R>(value: L): Either<L, R> => new this(value, undefined) as Either<L, R>;
 
     isRight = () => this.getLeft === undefined;
     isLeft = () => this.getRight === undefined;
@@ -26,12 +26,16 @@ export class Either<L, R> {
         if (this.isRight()) throw new Error(`Either getLeftError. Either is Right: ${this.getRight}`);
         return this.leftValue;}
 
-    flatMap = <T, S>(fn: (v: R) => Either<T, S>): Either<T|L, S> =>
-        this.isRight() ? fn(this.getRight) : Either.left<L, S>(this.getLeft);
+    // flatMap = <T, S>(fn: (v: R) => Either<T, S>): Either<T|L, S> =>
+    //     this.isRight() ? fn(this.getRight) : Either.left<L, S>(this.getLeft);
 
-    flatMapPromise = async <T, S>(fn: (v: R) => Promise<Either<T, S>>): Promise<Either<T|L, S>> =>
-        this.isRight() ? await fn(this.getRight) : Either.left<L, S>(this.getLeft);
+    // flatMapPromise = async <T, S>(fn: (v: R) => Promise<Either<T, S>>): Promise<Either<T|L, S>> =>
+    //     this.isRight() ? await fn(this.getRight) : Either.left<L, S>(this.getLeft);
     
-    map = <S>(fn: (v: R) => S) =>
-        this.flatMap<L, S>(v => Either.right(fn(v)));
+    // map = <S>(fn: (v: R) => S) =>
+    //     this.flatMap<L, S>(v => Either.right(fn(v)));
 }
+
+export class EitherRight<R> extends Either<never, R> {}
+
+export class EitherLeft<L> extends Either<L, never> {}
