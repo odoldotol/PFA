ece795ddaab7d7496f5480dd1e6515c98d832124
odoldotol <odoldotol@gmail.com>
2023-04-30 01:44:09 +0900

    Refac: UpperCasePipe/Test(Dedup)

--- market/src/common/pipe/upperCasePipe.spec.ts
+++ market/src/common/pipe/upperCasePipe.spec.ts
@@ -4,8 +4,30 @@ describe('UpperCasePipe', () => {
 
     let pipe: UpperCasePipe;
 
+    let testStr: {[k: `case${number}`]: string};
+    let successStr: {[k: `case${number}`]: string};
+    let testArr: {[k: `case${number}`]: Array<any>};
+    let successArr: {[k: `case${number}`]: Array<any>};
+    let testObj: {[k: `case${number}`]: object};
+    let successObj: {[k: `case${number}`]: object};
+
     beforeEach(() => {
         pipe = new UpperCasePipe();
+        
+        testStr = {
+            case1: 'abCdEFg',
+            case2: 'HiJKlmn',};
+        successStr = {
+            case1: 'ABCDEFG',
+            case2: 'HIJKLMN',};
+        testArr = {
+            case1: [testStr.case1, undefined, testStr.case2, null, 123],};
+        successArr = {
+            case1: [successStr.case1, undefined, successStr.case2, null, 123],};
+        testObj = {
+            case1: { a: testStr.case1, b: undefined, c: testStr.case2, d: null, e: 123 },};
+        successObj = {
+            case1: { a: successStr.case1, b: undefined, c: successStr.case2, d: null, e: 123 },};
     });
 
     it('should be defined', () => expect(pipe.transform).toBeDefined());
@@ -16,40 +38,38 @@ describe('UpperCasePipe', () => {
 
     describe('Case: string', () => {      
         it('should return upper case string', () =>
-            expect(pipe.transform('abCdEFg')).toBe('ABCDEFG'));});
+            expect(pipe.transform(testStr.case1)).toBe(successStr.case1));});
 
     describe('Case: array', () => {    
         it('should return upper case array', () => 
-            expect(pipe.transform(['abCdEFg', undefined, 'HiJKlmn', null, 123]))
-            .toEqual(['ABCDEFG', undefined, 'HIJKLMN', null, 123]));
+            expect(pipe.transform(testArr.case1)).toEqual(successArr.case1));
 
         it('multiple nested array', () => 
-            expect(pipe.transform([['abCdEFg', undefined, 'HiJKlmn', null, 123, ['abCdEFg', undefined, 'HiJKlmn', null, 123]]]))
-            .toEqual([['ABCDEFG', undefined, 'HIJKLMN', null, 123, ['ABCDEFG', undefined, 'HIJKLMN', null, 123]]]));
+            expect(pipe.transform([[...testArr.case1, testArr.case1]]))
+            .toEqual([[...successArr.case1, successArr.case1]]));
 
         it('combination in array', () =>
-            expect(pipe.transform(['abCdEFg', undefined, 'HiJKlmn', null, 123, { a: 'abCdEFg', b: undefined, c: 'HiJKlmn', d: null, e: 123 }]))
-            .toEqual(['ABCDEFG', undefined, 'HIJKLMN', null, 123, { a: 'ABCDEFG', b: undefined, c: 'HIJKLMN', d: null, e: 123 }]));
+            expect(pipe.transform([...testArr.case1, testObj.case1]))
+            .toEqual([...successArr.case1, successObj.case1]));
 
         it('combination in nested array', () =>
-            expect(pipe.transform([['abCdEFg', undefined, 'HiJKlmn', null, 123, ['abCdEFg', undefined, 'HiJKlmn', null, 123], { a: 'abCdEFg', b: undefined, c: 'HiJKlmn', d: null, e: 123 }]]))
-            .toEqual([['ABCDEFG', undefined, 'HIJKLMN', null, 123, ['ABCDEFG', undefined, 'HIJKLMN', null, 123], { a: 'ABCDEFG', b: undefined, c: 'HIJKLMN', d: null, e: 123 }]]));});
+            expect(pipe.transform([[...testArr.case1, testArr.case1, testObj.case1]]))
+            .toEqual([[...successArr.case1, successArr.case1, successObj.case1]]));});
 
     describe('Case: object', () => {
         it('should return upper case object', () =>
-            expect(pipe.transform({ a: 'abCdEFg', b: undefined, c: 'HiJKlmn', d: null, e: 123 }))
-            .toEqual({ a: 'ABCDEFG', b: undefined, c: 'HIJKLMN', d: null, e: 123 }));
+            expect(pipe.transform(testObj.case1)).toEqual(successObj.case1));
 
         it('multiple nested object', () =>
-            expect(pipe.transform({ a: { b: { c: { d: { a: 'abCdEFg', b: undefined, c: 'HiJKlmn', d: null, e: 123 } } } } }))
-            .toEqual({ a: { b: { c: { d: { a: 'ABCDEFG', b: undefined, c: 'HIJKLMN', d: null, e: 123 } } } } }));
+            expect(pipe.transform({ a: { b: { c: { d: testObj.case1 } } } }))
+            .toEqual({ a: { b: { c: { d: successObj.case1 } } } }));
 
         it('combination in object', () =>
-            expect(pipe.transform({ a: 'abCdEFg', b: undefined, c: ['HiJKlmn', null, 123] }))
-            .toEqual({ a: 'ABCDEFG', b: undefined, c: ['HIJKLMN', null, 123] }));
+            expect(pipe.transform(Object.assign(testObj.case1, {1: testArr.case1})))
+            .toEqual(Object.assign(successObj.case1, {1: successArr.case1})));
 
         it('combination in nested object', () =>
-            expect(pipe.transform({ a: { b: { c: { d: { a: 'abCdEFg', b: undefined, c: 'HiJKlmn', d: null, e: 123 } }, f: ['abCdEFg', undefined, 'HiJKlmn', null, 123] } } }))
-            .toEqual({ a: { b: { c: { d: { a: 'ABCDEFG', b: undefined, c: 'HIJKLMN', d: null, e: 123 } }, f: ['ABCDEFG', undefined, 'HIJKLMN', null, 123] } } }));});
+            expect(pipe.transform({ a: { b: { c: { d: testObj.case1 }, f: testArr.case1 } } }))
+            .toEqual({ a: { b: { c: { d: successObj.case1 }, f: successArr.case1 } } }));});
 
 });
