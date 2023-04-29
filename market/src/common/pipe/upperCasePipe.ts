ddf9292d8b0ea014b1760b84acea8c6567bb7adf
odoldotol <odoldotol@gmail.com>
2023-04-29 21:54:52 +0900

    Refac: UpperCasePipe/Green(commenting typeError) - define

--- market/src/common/pipe/upperCasePipe.ts
+++ market/src/common/pipe/upperCasePipe.ts
@@ -6,13 +6,13 @@ export class UpperCasePipe implements PipeTransform {
         if (value !== undefined) {
             if (Array.isArray(value)) {
                 return value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
-            } else if (value instanceof Object) {
-                return Object.entries(value).reduce((acc, [key, value]) => {
-                    if (typeof value === 'string') acc[key] = value.toUpperCase();
-                    else if (Array.isArray(value)) acc[key] = value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
-                    else acc[key] = value;
-                    return acc;
-                }, {});
+            // } else if (value instanceof Object) {
+            //     return Object.entries(value).reduce((acc, [key, value]) => {
+            //         if (typeof value === 'string') acc[key] = value.toUpperCase();
+            //         else if (Array.isArray(value)) acc[key] = value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
+            //         else acc[key] = value;
+            //         return acc;
+            //     }, {});
             } else if (typeof value === 'string') {
                 return value.toUpperCase();
             }
