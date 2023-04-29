a73532f36f29c9ed673f32030c295699f24aacfb
odoldotol <odoldotol@gmail.com>
2023-04-29 23:13:19 +0900

    Refac: UpperCasePipe/Green - object

--- market/src/common/pipe/upperCasePipe.ts
+++ market/src/common/pipe/upperCasePipe.ts
@@ -5,13 +5,16 @@ export class UpperCasePipe implements PipeTransform {
     transform = (value?: string | Array<any> | object) => {
         if (Array.isArray(value)) {
             return value.map((ele) => this.strToUpperCasePipe(ele));
-        // } else if (value instanceof Object) {
-        //     return Object.entries(value).reduce((acc, [key, value]) => {
-        //         if (typeof value === 'string') acc[key] = value.toUpperCase();
-        //         else if (Array.isArray(value)) acc[key] = value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
-        //         else acc[key] = value;
-        //         return acc;
-        //     }, {});
+        } else if (value instanceof Object) {
+            return Object.entries(value).reduce((acc, [key, value]) => {
+                // @ts-ignore
+                if (typeof value === 'string') acc[key] = value.toUpperCase();
+                // @ts-ignore
+                else if (Array.isArray(value)) acc[key] = value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
+                // @ts-ignore
+                else acc[key] = value;
+                return acc;
+            }, {});
         } else return this.strToUpperCasePipe(value);
     }
 
