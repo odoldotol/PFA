9a2ca572303206bd0341a0bf0f671ae82b21f07a
odoldotol <odoldotol@gmail.com>
2023-04-29 23:01:10 +0900

    Refac: UpperCasePipe/Dedup - str

--- market/src/common/pipe/upperCasePipe.ts
+++ market/src/common/pipe/upperCasePipe.ts
@@ -1,21 +1,19 @@
 import { PipeTransform } from "@nestjs/common";
 
 export class UpperCasePipe implements PipeTransform {
-    // TODO: Refac
-    transform(value?: string | Array<any> | object) {
-        if (value !== undefined) {
-            if (Array.isArray(value)) {
-                return value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
-            // } else if (value instanceof Object) {
-            //     return Object.entries(value).reduce((acc, [key, value]) => {
-            //         if (typeof value === 'string') acc[key] = value.toUpperCase();
-            //         else if (Array.isArray(value)) acc[key] = value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
-            //         else acc[key] = value;
-            //         return acc;
-            //     }, {});
-            } else if (typeof value === 'string') {
-                return value.toUpperCase();
-            }
-        }
+
+    transform = (value?: string | Array<any> | object) => {
+        if (Array.isArray(value)) {
+            return value.map((ele) => this.strToUpperCasePipe(ele));
+        // } else if (value instanceof Object) {
+        //     return Object.entries(value).reduce((acc, [key, value]) => {
+        //         if (typeof value === 'string') acc[key] = value.toUpperCase();
+        //         else if (Array.isArray(value)) acc[key] = value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
+        //         else acc[key] = value;
+        //         return acc;
+        //     }, {});
+        } else return this.strToUpperCasePipe(value);
     }
+
+    strToUpperCasePipe = (v: any) => typeof v === 'string' ? v.toUpperCase() : v;
 }
