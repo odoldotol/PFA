07627d04482e3d480cf26f564c9cd9583c627bdd
odoldotol <odoldotol@gmail.com>
2023-04-30 00:12:00 +0900

    Refac: UpperCasePipe/Dedup - object

--- market/src/common/pipe/upperCasePipe.ts
+++ market/src/common/pipe/upperCasePipe.ts
@@ -1,18 +1,18 @@
 import { PipeTransform } from "@nestjs/common";
+import * as F from '@fxts/core'
 
 export class UpperCasePipe implements PipeTransform {
 
-    transform = (value?: string | Array<any> | object) => {
+    transform = (value?: string | Array<any> | object): string | Array<any> | object => {
         if (Array.isArray(value)) {
             return value.map((ele) => this.strToUpperCasePipe(ele));
         } else if (value instanceof Object) {
-            const r: {[key: string]: any} = {}; 
-            return Object.entries(value).reduce((acc, [key, value]) => {
-                if (typeof value === 'string') acc[key] = value.toUpperCase();
-                else if (Array.isArray(value)) acc[key] = value.map((ele) => typeof ele === 'string' ? ele.toUpperCase() : ele);
-                else acc[key] = value;
-                return acc;
-            }, r);
+            return F.pipe(
+                value,
+                Object.entries,
+                F.map(([k,v]) => [k, this.transform(v)] as [string, any]),
+                F.fromEntries
+            );
         } else return this.strToUpperCasePipe(value);
     }
 
