996144d55089b9c84325d0df34412b9f658d9dec
odoldotol <odoldotol@gmail.com>
2023-05-04 12:11:23 +0900

    Refac: use method

--- market/src/common/class/either.ts
+++ market/src/common/class/either.ts
@@ -5,8 +5,8 @@ export abstract class Either<L, R> {
     static right = <L, R>(v: R): Either<L, R> => new EitherRight(v);
     static left = <L, R>(v: L): Either<L, R> => new EitherLeft(v);
 
-    isRight = () => this instanceof EitherRight;
-    isLeft = () => this instanceof EitherLeft;
+    isRight() { return this instanceof EitherRight; }
+    isLeft() { return this instanceof EitherLeft; }
 
     get getWhatever() {
         return this.eitherValue;}
@@ -17,8 +17,8 @@ export abstract class Either<L, R> {
     abstract flatMap<T, S>(fn: (v: R) => Either<T, S>): Either<L|T, S>;
     abstract flatMapAsync<T, S>(fn: (v: R) => Promise<Either<T, S>>): Promise<Either<L|T, S>>;
 
-    map = <S>(fn: (v: R) => S): Either<L, S> =>
-        this.flatMap(v => Either.right(fn(v)));
+    map<S>(fn: (v: R) => S): Either<L, S> {
+        return this.flatMap(v => Either.right(fn(v)));}
 }
 
 class EitherRight<R> extends Either<never, R> {
@@ -29,11 +29,11 @@ class EitherRight<R> extends Either<never, R> {
     get getLeft(): never {
         throw new Error(`Either getLeft Error. Either is Right, value: ${this.getWhatever}`);}
 
-    flatMap = <T, S>(fn: (v: R) => Either<T, S>) =>
-        fn(this.getWhatever);
+    flatMap<T, S>(fn: (v: R) => Either<T, S>) {
+        return fn(this.getWhatever);}
     
-    flatMapAsync = <T, S>(fn: (v: R) => Promise<Either<T, S>>) =>
-        fn(this.getWhatever);
+    flatMapAsync<T, S>(fn: (v: R) => Promise<Either<T, S>>) {
+        return fn(this.getWhatever);}
 }
 
 class EitherLeft<L> extends Either<L, never> {
@@ -44,9 +44,9 @@ class EitherLeft<L> extends Either<L, never> {
     get getLeft() {
         return this.getWhatever;}
     
-    flatMap = <T, S>(_fn: (v: never) => Either<T, S>) =>
-        Either.left<L, S>(this.getWhatever);
+    flatMap<T, S>(_fn: (v: never) => Either<T, S>) {
+        return Either.left<L, S>(this.getWhatever);}
     
-    flatMapAsync = async <T, S>(_fn: (v: never) => Promise<Either<T, S>>) =>
-        Either.left<L, S>(this.getWhatever);
+    async flatMapAsync<T, S>(_fn: (v: never) => Promise<Either<T, S>>) {
+        return Either.left<L, S>(this.getWhatever);}
 }
