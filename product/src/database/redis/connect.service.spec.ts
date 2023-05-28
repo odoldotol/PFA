726cd9d221ffde4b80878a7b2980e584b7ff7833
odoldotol <odoldotol@gmail.com>
2023-05-28 21:58:57 +0900

    Test: Getter client

--- product/src/database/redis/connect.service.spec.ts
+++ product/src/database/redis/connect.service.spec.ts
@@ -1,6 +1,26 @@
 import { ConnectService } from "./connect.service";
 
 describe('RedisConnectService', () => {
-    it.todo("연결 잘 되나")
-    it.todo("연결 에러시 처리 적절히 하나")
+
+    let service: ConnectService;
+
+    beforeEach(() => {
+        service = new ConnectService();
+    });
+
+    it("should be defined", () => {
+        expect(service).toBeDefined();
+    });
+
+    it.todo("연결 잘 되나");
+    it.todo("연결 에러시 처리 적절히 하나");
+
+    describe("Getter client", () => {
+        it("should be defined", () => {
+            expect(service.client).toBeDefined();
+        });
+
+        it.todo("should return redis client");
+    });
+
 });
