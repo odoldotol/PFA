41d66708eb68f0ec0e596f290fc2599f886902da
odoldotol <odoldotol@gmail.com>
2023-05-29 01:59:39 +0900

    Refac: iMCache.Repo 에서 Price 분리

--- product/src/database/iMCache/iMCache.repository.ts
+++ product/src/database/iMCache/iMCache.repository.ts
@@ -1,8 +1,6 @@
-import { CACHE_MANAGER, Inject, Injectable, Logger, OnApplicationBootstrap, OnModuleDestroy } from "@nestjs/common";
+import { CACHE_MANAGER, Inject, Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { Cache } from 'cache-manager';
-import { MarketDate } from "src/common/class/marketDate.class";
-import { CachedPrice } from "src/common/class/cachedPrice.class";
 import { curry, each, gte, head, isObject, isString, last, lte, map, not, nth, pipe, tap, toArray, toAsync, zip } from "@fxts/core";
 import { EnvironmentVariables } from "src/common/interface/environmentVariables.interface";
 import { EnvKey } from "src/common/enum/envKey.emun";
@@ -21,45 +19,11 @@ export class IMCacheRepository {
 
     // Todo: Refac - 캐시모듈 전체적으로 조악하다.
     
-    createPrice = ([symbol, price]: CacheSet<CachedPriceI>, ttl?: number) => 
-        this.setOne(symbol, new CachedPrice(price));
-
-    readPriceCounting = (symbol: TickerSymbol) => pipe(symbol,
-        this.readPrice,
-        this.counting);
-
-    updatePrice = async ([symbol, update]: CacheUpdateSet<CachedPriceI>) =>
-        this.update(await this.readPrice(symbol), update);
-
-    isGteMinCount = async (set: PSet) => pipe(head(set),
-        this.readPrice,
-        p => p && this.minThreshold <= p.count);
-
-    private readPrice = (symbol: TickerSymbol): Promise<CachedPriceI | null> => pipe(symbol,
-        this.getValue,
-        this.passCachedPrice);
-    
-    private passCachedPrice = (v: CacheValue | undefined) => v instanceof CachedPrice ? v : null;
-    
-    private counting = (v: CachedPriceI | null) => v && v.counting && v.counting(); // Todo: Refac
-    
     private copy = <T>(v: T): T => v && Object.assign({copy: true}, v);
-
-    // Todo: Refac
-    private setOne<T>(cacheSet: CacheSet<T>): Promise<T>
-    private setOne<T>(key: CacheKey, value: T): Promise<T>
-    private setOne<T>(key: CacheKey, value: T, ttl: number): Promise<T>
-    private setOne<T>(arg: CacheKey | CacheSet<T>, value?: T, ttl?: number) {
-        return Array.isArray(arg) ? 
-            arg[2] === undefined ? this.cacheManager.set<T>(arg[0], arg[1]) : this.cacheManager.set<T>(arg[0], arg[1], arg[2])
-            : this.cacheManager.set<T>(arg, value!, ttl!);
-    }
 
     private getKeyValueSet = async (key: CacheKey) => [ key, await this.getValue(key) ] as CacheSet<CacheValue>;
 
     private getValue = (key: CacheKey): Promise<CacheValue | undefined> => this.cacheManager.get(key);
-
-    private update = <T>(v: T, update: Partial<T>): T => v && Object.assign(v, update);
 
     deleteOne = (key: CacheKey) => this.cacheManager.del(key);
 
