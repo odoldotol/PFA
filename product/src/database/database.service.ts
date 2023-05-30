4d7db282f19d60833408bed5e67e1ac23d66cf35
odoldotol <odoldotol@gmail.com>
2023-05-31 00:11:53 +0900

    Rename

--- product/src/database/database.service.ts
+++ product/src/database/database.service.ts
@@ -14,16 +14,16 @@ export class DatabaseService {
     constructor(
         private readonly appMemSrv: InMemoryService,
         private readonly cacheBackupSrv: BackupService,
-        private readonly marketDateRepo: MarketDateService,
-        private readonly priceRepo: PriceService
+        private readonly marketDateSrv: MarketDateService,
+        private readonly priceSrv: PriceService
     ) {}
     
     createCcPriceStatusWithRP = (rP: RequestedPrice) => rP.status_price &&
-        this.marketDateRepo.create([rP.status_price.ISO_Code, MarketDate.fromSpDoc(rP.status_price)]);
-    createCcPrice = this.priceRepo.create;
-    readCcStatusPrice = this.marketDateRepo.read;
-    readCcPriceCounting = this.priceRepo.read_with_counting;
-    updateCcPrice = this.priceRepo.update;
+        this.marketDateSrv.create([rP.status_price.ISO_Code, MarketDate.fromSpDoc(rP.status_price)]);
+    createCcPrice = this.priceSrv.create;
+    readCcStatusPrice = this.marketDateSrv.read;
+    readCcPriceCounting = this.priceSrv.read_with_counting;
+    updateCcPrice = this.priceSrv.update;
     
     cacheRecovery = this.cacheBackupSrv.localFileCacheRecovery;
     getAllCcKeys = this.appMemSrv.getAllKeys;
@@ -31,12 +31,12 @@ export class DatabaseService {
     // TODO: 각 업데이트 Asset이 해당 Sp 에 속한게 맞는지 검사하고 있지 않다. 이거 문제될 가능성 있는지 찾아봐.
     updatePriceBySpPSets = (initSet: SpPSets) => pipe(initSet,
         this.setSpAndReturnPSets, toAsync,
-        partition(this.priceRepo.isGteMinCount), ([ updatePSets, deletePSets ]) => (
+        partition(this.priceSrv.isGteMinCount), ([ updatePSets, deletePSets ]) => (
             pipe(updatePSets,
                 map(this.toCacheUpdateSet(head(initSet))),
                 each(this.updateCcPrice)),
             pipe(deletePSets,
-                each(a => this.priceRepo.delete(head(a))))))
+                each(a => this.priceSrv.delete(head(a))))))
     .then(() => this.logger.verbose(`${head(head(initSet))} : Regular Updated`));
 
     cacheHardInit = (initSet: SpPSets) => pipe(initSet,
@@ -45,7 +45,7 @@ export class DatabaseService {
         each(this.createCcPrice));
 
     private setSpAndReturnPSets = (initSet: SpPSets) => pipe(initSet,
-        tap(set => this.marketDateRepo.create(head(set))),
+        tap(set => this.marketDateSrv.create(head(set))),
         last);
 
     // Todo: Refac - toCacheUpdateSet, toCachedPriceSet 중복함수
