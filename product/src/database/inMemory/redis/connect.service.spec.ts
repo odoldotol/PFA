b8a8bba705a9e37f48c14507d1c4991386322a0d
odoldotol <odoldotol@gmail.com>
2023-06-05 04:50:27 +0900

    Test(Red): 준비된 redisClient를 리턴하는지 / Test(todo)

--- product/src/database/inMemory/redis/connect.service.spec.ts
+++ product/src/database/inMemory/redis/connect.service.spec.ts
@@ -12,15 +12,20 @@ describe('RedisConnectService', () => {
         expect(service).toBeDefined();
     });
 
-    it.todo("연결 잘 되나");
+    it.todo("모듈이 생성될때 연결 잘 하나");
     it.todo("연결 에러시 처리 적절히 하나");
 
     describe("Getter client", () => {
+
         it("should be defined", () => {
             expect(service.client).toBeDefined();
         });
 
-        it.todo("should return redis client");
+        it("should return redis client in ready.", () => {
+            expect(service.client.isReady).toBeTruthy();
+        });
     });
+
+    it.todo("연결에 문제가 생김에 따른 대처");
 
 });
