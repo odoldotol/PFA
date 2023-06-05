bb916caf25e9126dd714f4331faed6f72695fc9b
odoldotol <odoldotol@gmail.com>
2023-06-05 10:21:10 +0900

    Test(Green): onApplicationShutdown 멱등성 보장

--- product/src/database/inMemory/redis/connect.service.spec.ts
+++ product/src/database/inMemory/redis/connect.service.spec.ts
@@ -25,24 +25,26 @@ describe('RedisConnectService', () => {
     it.todo("모듈이 생성될때 연결 잘 하나");
     it.todo("연결 에러시 처리 적절히 하나");
     
-    describe("앱이 종료될떄 연결을 연결을 끊자.", () => {
-        it("onApplicationShutdown 매서드는 연결을 끊는다", async () => {
-            await service.onApplicationShutdown();
-            expect(service.client.isReady).toBeFalsy();
-            expect(service.client.isOpen).toBeFalsy();
-        });
-    });
+    describe("앱이 종료될떄 연결을 끊자.", () => {
+        describe("onApplicationShutdown 매서드", () => {
+            it("연결을 끊는다", async () => {
+                await service.onApplicationShutdown();
+                expect(service.client.isReady).toBeFalsy();
+                expect(service.client.isOpen).toBeFalsy();});
+
+            it("이미 연결이 닫혀있어도 실행에 문제가 없고 결과가 같다.", async () => {
+                await service.onApplicationShutdown();
+                await service.onApplicationShutdown();
+                expect(service.client.isReady).toBeFalsy();
+                expect(service.client.isOpen).toBeFalsy();});});});
 
 
     describe("Getter client", () => {
-
         it("should be defined", () => {
-            expect(service.client).toBeDefined();
-        });
+            expect(service.client).toBeDefined();});
 
         it("should return redis client in ready.", () => {
-            expect(service.client.isReady).toBeTruthy();
-        });
+            expect(service.client.isReady).toBeTruthy();});
     });
 
     it.todo("연결에 문제가 생김에 따른 대처");
