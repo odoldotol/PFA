2ac9bf788994e10cb5e07e8f717b0d2adf2fed17
odoldotol <odoldotol@gmail.com>
2023-06-05 11:08:02 +0900

    Test(Green): RedisService should be defined

--- product/src/database/inMemory/redis/redis.service.spec.ts
+++ product/src/database/inMemory/redis/redis.service.spec.ts
@@ -1,5 +1,27 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { ConnectService } from "./connect.service";
 import { RedisService } from "./redis.service";
 
 describe("RedisService", () => {
+
+    let module: TestingModule;
+    let service: RedisService;
+
+    beforeAll(async ()  => {
+        module = await Test.createTestingModule({
+            providers: [ConnectService, RedisService]
+        }).compile();
+
+        service = module.get<RedisService>(RedisService);
+
+        await module.init();
+    });
+
+    beforeAll(async () => {
+        await module.close();
+    });
+
+    it("should be defined", () => {
+        expect(service).toBeDefined();});
     
 });
