a9be0e131ee8c179588f8151e6dd6e8cbe728dc5
odoldotol <odoldotol@gmail.com>
2023-06-05 18:26:07 +0900

    Test(Red): getAllKeys / Test: Redis 인스턴스를 이용하도록

--- product/src/database/inMemory/redis/redis.service.spec.ts
+++ product/src/database/inMemory/redis/redis.service.spec.ts
@@ -1,11 +1,17 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ConnectService } from "./connect.service";
 import { RedisService } from "./redis.service";
+import * as F from '@fxts/core'
 
 describe("RedisService", () => {
 
     let module: TestingModule;
     let service: RedisService;
+    let client: ConnectService["client"];
+
+    const TEST_KEY_PREFIX = "pfa:unittest:";
+    const testKeyValuePairCount = 500;
+    let testKeyValueMap: Map<string, string>;
 
     beforeAll(async ()  => {
         module = await Test.createTestingModule({
@@ -13,19 +19,49 @@ describe("RedisService", () => {
         }).compile();
 
         service = module.get<RedisService>(RedisService);
+        client = module.get<ConnectService>(ConnectService).client;
 
-        await module.init();
-    });
+        await module.init();});
 
     afterAll(async () => {
-        await module.close();
-    });
+        await module.close();});
+
+
+    beforeEach(async () => {
+        testKeyValueMap = new Map<string, string>(F.pipe(
+            F.range(testKeyValuePairCount),
+            F.map((i) => [`key${i}`, `value${i}`])
+        ));
+
+        const msetCommand = ["MSET"];
+        testKeyValueMap.forEach((value, key) => {
+            msetCommand.push(TEST_KEY_PREFIX+key, value);
+        });
+
+        if (msetCommand.length%2 === 1) await client.sendCommand(msetCommand);
+        else throw new Error("MSET Command must have key-value pair.");});
+
+    afterEach(async () => {
+        const allTestKeys: string[] = await client.sendCommand([
+            "KEYS", TEST_KEY_PREFIX+"*"
+        ]);
+        
+        await client.sendCommand([
+            "DEL", ...allTestKeys
+        ]);});
+
 
     it("should be defined", () => {
         expect(service).toBeDefined();});
     
     describe('getAllKeys', () => {
-        it.todo("모든 키를 배열로 반환.");
+        it("모든 키를 배열로 반환.", async () => {
+            const allKeys = await service.getAllKeys();
+            allKeys.forEach((key) => {
+                if (key.slice(0, TEST_KEY_PREFIX.length) === TEST_KEY_PREFIX) testKeyValueMap.delete(key);
+            });
+            expect(testKeyValueMap.size).toBe(0);
+        });
     });
 
     describe('setCache', () => {
