e7586c1bb849140010394c94903c1fa3317551a4
odoldotol <odoldotol@gmail.com>
2023-06-05 16:09:14 +0900

    Test(todo)

--- product/src/database/inMemory/redis/redis.service.spec.ts
+++ product/src/database/inMemory/redis/redis.service.spec.ts
@@ -24,4 +24,24 @@ describe("RedisService", () => {
     it("should be defined", () => {
         expect(service).toBeDefined();});
     
+    describe('getAllKeys', () => {
+        it.todo("모든 키를 배열로 반환.");
+    });
+
+    describe('setCache', () => {
+        it.todo("key, value, ttl 튜플 받아서 set 한다.");
+        it.todo("성공시 value, 실패시 null 반환."); // 실패? null 반환?
+        it.todo("ttl(초) 이후에 만료되야 한다.");
+    });
+
+    describe('deleteCache', () => {
+        it.todo("key 하나를 받아서 삭제한다.");
+        it.todo("성공시 true, 실패시 false 반환.");
+    });
+
+    describe('getValue', () => {
+        it.todo("key 하나를 받아서 value 하나를 반환.");
+        it.todo("없으면 null 반환.");
+    });
+    
 });
