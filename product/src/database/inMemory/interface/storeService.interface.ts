00f8d774e4a8fa231c3c723b63b07493ed2c328e
odoldotol <odoldotol@gmail.com>
2023-06-06 12:57:10 +0900

    Refac: storeServiceInterface 최소화

--- product/src/database/inMemory/interface/storeService.interface.ts
+++ product/src/database/inMemory/interface/storeService.interface.ts
@@ -1,6 +1,3 @@
 interface InMemoryStoreServiceI {
     getAllKeys(): Promise<string[]>;
-    setCache<T>(cacheSet: CacheSet<T>): Promise<T>;
-    deleteCache(key: string): Promise<boolean>;
-    getValue(key: string): Promise<any>;
 }
