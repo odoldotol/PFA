b21d61b5bb8e6647fe54f6c81707481c7e6ff1d1
odoldotol <odoldotol@gmail.com>
2023-06-08 07:10:44 +0900

    Feat(Green): redisRepository - copy

--- product/src/database/inMemory/redis/redis.repository.ts
+++ product/src/database/inMemory/redis/redis.repository.ts
@@ -23,7 +23,8 @@ export class RedisRepository<T> implements InMemoryRepositoryI<T> {
 
     get = (key: string) => Promise.resolve(null);
 
-    copy = (v: T | null ) => null;
+    // 사용하지 않을 예정인 매서드
+    copy = (v: T | null ) => v && new this.schema.constructorClass(v);
 
     // Todo: 키 타입(프리픽스s+키바디), 키 프리픽스 타입(string+";"), 키 바디 타입(':' 있으면 안됨) 만들기
     private makeKey = (keyBody: string) => this.KEY_PREFIX + keyBody;
