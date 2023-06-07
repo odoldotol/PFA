ad9a9bf489c6722cc0fedbaf5908727292422033
odoldotol <odoldotol@gmail.com>
2023-06-07 23:10:20 +0900

    Dedup / Comment

--- product/src/database/inMemory/redis/redis.repository.ts
+++ product/src/database/inMemory/redis/redis.repository.ts
@@ -13,9 +13,9 @@ export class RedisRepository<T> implements InMemoryRepositoryI<T> {
         private readonly schema: InMemorySchema,
     ) {}
 
-    createOne = (key: string, value: T) => this.redisSrv.setOne([this.KEY_PREFIX+key, value], { expireSec: this.TTL, ifNotExist: true });
+    createOne = (key: string, value: T) => this.redisSrv.setOne([this.makeKey(key), value], { expireSec: this.TTL, ifNotExist: true });
 
-    findOne = (key: string) => this.redisSrv.getOne(this.KEY_PREFIX+key);
+    findOne = (key: string) => this.redisSrv.getOne(this.makeKey(key));
 
     updateOne = (key: string, update: Partial<T>) => Promise.resolve(null);
 
@@ -24,5 +24,8 @@ export class RedisRepository<T> implements InMemoryRepositoryI<T> {
     get = (key: string) => Promise.resolve(null);
 
     copy = (v: T | null ) => null;
+
+    // Todo: 키 타입(프리픽스s+키바디), 키 프리픽스 타입(string+";"), 키 바디 타입(':' 있으면 안됨) 만들기
+    private makeKey = (keyBody: string) => this.KEY_PREFIX + keyBody;
 
 }
