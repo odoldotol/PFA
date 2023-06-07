76fd3100b192e8b7e997225d69c75820ccfbb1aa
odoldotol <odoldotol@gmail.com>
2023-06-07 18:22:20 +0900

    Feat: RedisServiceInterface.setOne - setOptions

--- product/src/database/inMemory/redis/interface/service.interface.ts
+++ product/src/database/inMemory/redis/interface/service.interface.ts
@@ -1,5 +1,20 @@
+type MaximumOneOf<T, K extends keyof T = keyof T> = K extends keyof T ? {
+    [P in K]?: T[K];
+} & Partial<Record<Exclude<keyof T, K>, never>> : never;
+
+type SetTTL = {
+    expireSec: number;
+};
+
+type SetIf = MaximumOneOf<{
+    ifNotExist: true;
+    ifExist: true;
+}>;
+
+type SetOptions = SetTTL & SetIf;
+
 interface RedisServiceI {
-    setOne: <T>([key, value, ttl]: [string, T, number]) => Promise<T|null>;
+    setOne: <T>([key, value]: [string, T], setOptions: SetOptions) => Promise<T|null>;
     deleteOne: (key: string) => Promise<any>;
     getOne: (key: string) => Promise<any>;
 }
