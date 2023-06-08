d9e6e39c4821095da46fb7ba1e0a34c85c88381d
odoldotol <odoldotol@gmail.com>
2023-06-08 09:56:52 +0900

    Feat(Green): redisRepository - updateOne - findOne And Update

--- product/src/database/inMemory/redis/redis.repository.ts
+++ product/src/database/inMemory/redis/redis.repository.ts
@@ -22,11 +22,11 @@ export class RedisRepository<T> implements InMemoryRepositoryI<T> {
         this.redisSrv.getOne(this.makeKey(key)),
         this.valueFactory);
 
-    updateOne = (key: string, update: Partial<T>) => Promise.resolve(null);
+    updateOne = (key: string, update: Partial<T>) => this.findOne(key);
 
     deleteOne = (key: string) => Promise.resolve(true);
 
-    private valueFactory = (v: T | null ) => v && new this.schema.constructorClass(v);
+    private valueFactory = (v: T | null ) => v && new this.schema.constructorClass(v) as T;
 
     // Todo: 키 타입(프리픽스s+키바디), 키 프리픽스 타입(string+";"), 키 바디 타입(':' 있으면 안됨) 만들기
     private makeKey = (keyBody: string) => this.KEY_PREFIX + keyBody;
