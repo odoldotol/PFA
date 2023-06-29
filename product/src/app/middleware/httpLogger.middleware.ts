3dd5752e38cc384283e096d331eee7c325861517
odoldotol <odoldotol@gmail.com>
2023-06-30 06:43:22 +0900

    fix: httpLogger listener closure memory leak

--- product/src/app/middleware/httpLogger.middleware.ts
+++ product/src/app/middleware/httpLogger.middleware.ts
@@ -8,15 +8,16 @@ export class HttpLoggerMiddleware implements NestMiddleware {
     private readonly logger = new Logger('HttpLogger');
 
     async use(req: Request, res: Response, next: NextFunction) {
-        req.url
         const now = Date.now();
         const { method, originalUrl } = req;
-        res.on('finish', () => {
+        let listener: (() => void) | null = () => {
+            listener = null;
             const responseTime = Date.now() - now;
             const { statusCode } = res;
             if (originalUrl === '/health' && method === 'GET' && statusCode === 200) return;
             this.logger.log(`${statusCode} | ${responseTime}ms | ${method} | ${originalUrl}`);
-        });
+        };
+        res.on('finish', listener);
         next();
     }
     
