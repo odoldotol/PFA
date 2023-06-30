7c5cd8a1da3aa91364971e0db5c9eb3a09dd7bce
odoldotol <odoldotol@gmail.com>
2023-07-01 01:28:56 +0900

    feat: wait for dependency server when app Initilize

--- market/src/market/child-api/child-api.module.ts
+++ market/src/market/child-api/child-api.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
+import { ConnectionService } from './connection.service';
 import { ChildApiService } from './child-api.service';
 import { EnvKey } from 'src/common/enum/envKey.emun';
 import { EnvironmentVariables } from 'src/common/interface/environmentVariables.interface';
 
 @Module({
-    imports: [
-        HttpModule.registerAsync({
-            imports: [ConfigModule],
-            useFactory: async (configService: ConfigService<EnvironmentVariables>) => ({
-                baseURL: configService.get(EnvKey.Docker_childApiBaseUrl, 'http://localhost:8001', { infer: true }),
-                timeout: configService.get(EnvKey.ChildApiTimeout, 30000, { infer: true }),
-            }),
-            inject: [ConfigService]})],
-    providers: [ChildApiService],
-    exports: [ChildApiService]
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService<EnvironmentVariables>) => ({
+        baseURL: configService.get(EnvKey.Docker_childApiBaseUrl, 'http://localhost:8001', { infer: true }),
+        timeout: configService.get(EnvKey.ChildApiTimeout, 30000, { infer: true }),
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  providers: [
+    ConnectionService,
+    ChildApiService
+  ],
+  exports: [ChildApiService]
 })
 export class ChildApiModule {}
--- product/src/market/market-api/market-api.module.ts
+++ product/src/market/market-api/market-api.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
+import { ConnectionService } from './connection.service';
 import { MarketApiService } from './market-api.service';
 import { EnvironmentVariables } from 'src/common/interface/environmentVariables.interface';
 import { EnvKey } from 'src/common/enum/envKey.emun';
@@ -8,13 +9,18 @@ import { EnvKey } from 'src/common/enum/envKey.emun';
 @Module({
   imports: [
     HttpModule.registerAsync({
-        imports: [ConfigModule],
-        useFactory: async (configService: ConfigService<EnvironmentVariables>) => ({
-            baseURL: configService.get(EnvKey.Docker_marketApiBaseUrl, 'http://localhost:6001'),
-            timeout: configService.get(EnvKey.MarketApiTimeout, 10000)
-        }),
-        inject: [ConfigService]})],
-  providers: [MarketApiService],
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService<EnvironmentVariables>) => ({
+          baseURL: configService.get(EnvKey.Docker_marketApiBaseUrl, 'http://localhost:6001'),
+          timeout: configService.get(EnvKey.MarketApiTimeout, 10000)
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  providers: [
+    ConnectionService,
+    MarketApiService
+  ],
   exports: [MarketApiService]
 })
 export class MarketApiModule {}
