e6f25d8ddb2dfd5ebef28fe6508a1a3a76bd3fa1
odoldotol <odoldotol@gmail.com>
2023-07-16 23:16:07 +0900

    module init

--- market/src/updater/updater.module.ts
+++ market/src/updater/updater.module.ts
@@ -1,16 +1,18 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { UpdaterController } from './updater.controller';
 import { UpdaterService } from './updater.service';
 import { MarketModule } from 'src/market/market.module';
 import { DBModule } from 'src/database/database.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 import { ProductApiModule } from 'src/product-api/product-api.module';
 
 @Module({
   imports: [
     MarketModule,
     DBModule,
-    ProductApiModule],
+    SchedulerModule,
+    ProductApiModule
+  ],
   controllers: [UpdaterController],
   providers: [UpdaterService],
   exports: [UpdaterService]
