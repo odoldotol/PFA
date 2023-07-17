16b2ba43977f786d6c0a7e1a4287023dbc52e9fb
odoldotol <odoldotol@gmail.com>
2023-07-17 16:07:07 +0900

    Green(Mock)

--- market/src/updater/scheduler.service.ts
+++ market/src/updater/scheduler.service.ts
@@ -4,7 +4,16 @@ import { UpdaterJob } from "./class/job";
 @Injectable()
 export class UpdaterSchedulerService {
 
-  addSchedule(exchange: string, nextTime: Date, job: any) {
-    return new UpdaterJob();
+  // Todo: exchange 리팩터링 이후, Map key 를 리팩터링 된 exchange 에 맞게 변경 하기
+  private readonly updaterJobMap: Map<string, UpdaterJob> = new Map();
+
+  public addSchedule(exchange: string, nextTime: Date, job: any) {
+    const updaterJob = new UpdaterJob()
+    this.updaterJobMap.set(exchange, updaterJob);
+    return updaterJob;
+  }
+
+  public getAllJob() {
+    return this.updaterJobMap;
   }
 }
