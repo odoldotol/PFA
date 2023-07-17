d18fe1864be0acc2d2481c6d13cf2dd346bb0a4f
odoldotol <odoldotol@gmail.com>
2023-07-17 15:47:12 +0900

    Test(Red) - addSchedule

--- market/src/database/temp-exchangeArr.const.ts
+++ market/src/database/temp-exchangeArr.const.ts
@@ -1,3 +1,5 @@
+// Todo: Production 환경이 아닌 특정 개발 환경에서만 사용되는 임시 데이터
+// ts 파일 보다는 다른 데이터 파일 형식으로 두고 읽어서 몽고디비에 인서트하고 시작하도록 리팩터링하자.
 export const TEMP_EXCHANGES = [
   { ISO_Code: 'BVMF', ISO_TimezoneName: 'America/Sao_Paulo' },
   { ISO_Code: 'XFRA', ISO_TimezoneName: 'Europe/Berlin' },
--- market/src/updater/scheduler.service.spec.ts
+++ market/src/updater/scheduler.service.spec.ts
@@ -1,9 +1,32 @@
+import { Test, TestingModule } from "@nestjs/testing";
 import { UpdaterSchedulerService } from "./scheduler.service";
+import { UpdaterJob } from "./class/job";
 
 describe("SchedulerService", () => {
 
-  describe("새로운 업데이터 스케줄 등록 메서드", () => {
-    it.todo("거래소, 시간, 업데이트작업함수 받아서 스케줄 만들고 잡 반환");
+  let service: UpdaterSchedulerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UpdaterSchedulerService],
+    }).compile();
+
+    service = module.get<UpdaterSchedulerService>(UpdaterSchedulerService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  describe("addSchedule: 새로운 업데이터 스케줄 등록", () => {
+    it("Exchange객체(임시: ISO_Code), 다음시간, 업데이트작업함수 받아서 스케줄 만들고 해당 Exchange 의 UpdaterJob 반환", () => {
+      const job = service.addSchedule("XXXX", new Date(), () => {});
+      expect(job).toBeInstanceOf(UpdaterJob);
+    });
   });
 
   describe("모든 업데이터 스케줄 조회 메서드", () => {
