70c4686a685600fda4b3dfb51aa1a2d39f120bfd
odoldotol <odoldotol@gmail.com>
2023-08-15 22:15:49 +0900

    Test - exchangeService.exists

--- market/src/database/exchange/exchange.service.spec.ts
+++ market/src/database/exchange/exchange.service.spec.ts
@@ -67,4 +67,12 @@ describe('ExchangeService', () => {
       });
     });
   });
+
+  describe('exists', () => {
+    it('should return boolean if exists or not', async () => {
+      expect(await service.exists({ ISO_Code: mockKoreaExchange.ISO_Code })).toBe(false);
+      await service.createOne(mockKoreaExchange);
+      expect(await service.exists({ ISO_Code: mockKoreaExchange.ISO_Code })).toBe(true);
+    });
+  });
 });
