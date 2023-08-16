12690a303579dee25b713f8529424e133dac9d24
odoldotol <odoldotol@gmail.com>
2023-08-16 21:46:07 +0900

    Test - finAssetService.createMany

--- market/src/database/financialAsset/financialAssets.service.spec.ts
+++ market/src/database/financialAsset/financialAssets.service.spec.ts
@@ -4,9 +4,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import postgresConfig from "src/config/postgres.config";
 import { DataSource } from "typeorm";
 import { Exchange } from "../exchange/exchange.entity";
+import { ExchangeService } from "../exchange/exchange.service";
+import { mockKoreaExchange, mockNewYorkStockExchange } from "../exchange/mock/exchange.mock";
 import { TypeOrmConfigService } from "../postgres/typeormConfig.service";
 import { FinancialAsset } from "./financialAsset.entity";
 import { FinancialAssetService } from "./financialAsset.service";
+import { mockApple, mockSamsungElec, mockUsaTreasuryYield10y } from "./mock/asset.mock";
 
 describe('FinancialAssetsService', () => {
   let service: FinancialAssetService;
@@ -32,6 +35,10 @@ describe('FinancialAssetsService', () => {
 
     service = module.get<FinancialAssetService>(FinancialAssetService);
     dataSource = module.get<DataSource>(DataSource);
+    
+    const exchangeSrv = module.get<ExchangeService>(ExchangeService)
+    await exchangeSrv.createOne(mockKoreaExchange);
+    await exchangeSrv.createOne(mockNewYorkStockExchange);
   });
 
   afterEach(async () => {
@@ -43,5 +50,16 @@ describe('FinancialAssetsService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('createMany', () => {
+    it('should create records in financial_assets table', async () => {
+      await service.createMany([
+        mockApple,
+        mockSamsungElec,
+        mockUsaTreasuryYield10y
+      ]);
+      const result = await dataSource.query('SELECT * FROM financial_assets');
+      expect(result.length).toBe(3);
+    });
+  });
 
 });
