a189fda779421ef673ef76f66e5c6b28fc25f747
odoldotol <odoldotol@gmail.com>
2023-08-16 19:36:37 +0900

    finAsset entity

--- market/src/database/financialAsset/financialAsset.entity.ts
+++ market/src/database/financialAsset/financialAsset.entity.ts
@@ -4,25 +4,35 @@ import { Exchange } from '../exchange/exchange.entity';
 @Entity({ name: 'financial_assets' })
 @Index(['exchange', 'symbol'], { unique: true })
 export class FinancialAsset {
-  @PrimaryColumn({ type: 'char', length: 4 })
+  @PrimaryColumn({ type: 'char', length: 4, name: 'symbol' }) // Todo: 4글자 미만이면 인서트 막기
   symbol!: string;
 
-  @Column({ type: 'varchar', length: 10 })
+  @Column({ type: 'varchar', length: 10, name: 'quotetype' })
   quoteType!: string;
 
-  @Column({ type: 'varchar', length: 30 })
+  @Column({ type: 'varchar', length: 30, name: 'shortname' })
   shortName!: string;
 
-  @Column({ type: 'varchar', length: 50, nullable: true })
-  longName!: string;
+  @Column({ type: 'varchar', length: 50, name: 'longname', nullable: true })
+  longName?: string;
 
-  @ManyToOne(() => Exchange)
+  @ManyToOne(() => Exchange, { nullable: true })
   @JoinColumn({ name: 'exchange', referencedColumnName: 'ISO_Code' })
-  exchange!: string;
+  exchange?: string;
 
-  @Column({ type: 'char', length: 3 })
+  @Column({ type: 'char', length: 3, name: 'currency' })
   currency!: string;
 
-  @Column({ type: 'double precision' })
+  @Column({ type: 'double precision', name: 'regularmarketlastclose' })
   regularMarketLastClose!: number;
 }
+
+export type RawFinancialAsset = {
+  symbol: string;
+  quotetype: string;
+  shortname: string;
+  longname: string | null;
+  exchange: string | null;
+  currency: string;
+  regularmarketlastclose: number;
+};
