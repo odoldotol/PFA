c41844dcad4b1fa00513275bb661b5dfb94664b8
odoldotol <odoldotol@gmail.com>
2023-11-06 18:30:21 +0900

    build: postgres

--- market/src/config/postgres.config.ts
+++ market/src/config/postgres.config.ts
@@ -1,16 +1,11 @@
 export default () => {
 
   if (process.env.RACK_ENV === 'production') return {};
-  if (process.env.RACK_ENV === 'development') {
-    // docker test, development
-    return {};
-  } else {
-    // local test, development
-    return {
-      PG_HOST: '127.0.0.1',
-      PG_USERNAME: 'test',
-      PG_PASSWORD: 'test',
-      PG_DATABASE: 'test'
-    };
-  }
+  
+  return {
+    PG_HOST: process.env.RACK_ENV === 'development' ? 'market-postgres' : '127.0.0.1',
+    PG_USERNAME: 'test',
+    PG_PASSWORD: 'test',
+    PG_DATABASE: 'test'
+  };
 };
