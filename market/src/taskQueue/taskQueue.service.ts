025936676c97518a1938cc529396a0d631789388
odoldotol <odoldotol@gmail.com>
2024-01-07 08:26:22 +0900

    Refac(TaskQueue): ignore Task errors inside

--- market/src/taskQueue/taskQueue.service.ts
+++ market/src/taskQueue/taskQueue.service.ts
@@ -29,7 +29,7 @@ export class TaskQueueService<T = any>
     }
   }
 
-  public runTask(task: Task<T>): Promise<T> {
+  public runTask(task: Task<T>): ReturnType<Task<T>> {
     return new Promise((resolve, reject) => {
       const taskWrapper: Task<T> = () => {
         const taskPromise = task();
@@ -39,8 +39,8 @@ export class TaskQueueService<T = any>
 
       if (this.consumerQueue.length !== 0) {
         // there is a sleeping consumer available, use it to run our task
-        const consumer = this.consumerQueue.shift()!;
-        consumer(taskWrapper);
+        const getNextTaskResolver = this.consumerQueue.shift()!;
+        getNextTaskResolver(taskWrapper);
       } else {
         // all consumers are busy, enqueue the task
         this.taskQueue.push(taskWrapper);
@@ -49,11 +49,11 @@ export class TaskQueueService<T = any>
   }
 
   // 비동기적인 재귀 (재귀적 프로미스 해결의 메모리 누수 버그 주의)
-  private consumer() {
+  private consumer(): Promise<void> {
     return new Promise<void>(resolve => {
       this.getNextTask()
-      .then(task => task())
-      .catch(err => console.error(err)) // Todo: 에러 처리
+      .then(taskWrapper => taskWrapper())
+      .catch(_error => {}) // Task 에 대한 에러는 여기서는 무시하고 runTask 를 통해 던져져서 외부에서 처리한다.
       .finally(() => {
         this.consumer();
         resolve(); // resolve(this.consumer()) 는 무한 재귀 프로미스 해결 체인의 메모리 누수 버그를 일으킴.
@@ -61,13 +61,13 @@ export class TaskQueueService<T = any>
     });
   }
 
-  private getNextTask() {
-    return new Promise<Task<T>>((resolve) => {
+  private getNextTask(): Promise<Task<T>> {
+    return new Promise(resolve => {
       if (this.taskQueue.length !== 0) {
-        return resolve(this.taskQueue.shift()!);
+        resolve(this.taskQueue.shift()!);
+      } else {
+        this.consumerQueue.push(resolve);
       }
-
-      this.consumerQueue.push(resolve);
     });
   }
 
