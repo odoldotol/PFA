c47a6260d67751f8160ecca1aa80729ea6ff60be
odoldotol <odoldotol@gmail.com>
2024-12-20 01:44:32 +0900

    fix(KakaoChatbot/Lapiki): exception button

--- product/src/kakaoChatbot/const/index.ts
+++ product/src/kakaoChatbot/const/index.ts
@@ -5,9 +5,9 @@ export * from './throttleOptions';
  *  카카오챗봇 관련 정적 데이터(텍스트, 버튼, 블록 등등)를 분리하기전까지만 임시로 사용.
  */
 export const chatbotListMenuButtons = {
-  inquireAsset: {
-    title: "찾아 보기",
-  },
+  // inquireAsset: {
+  //   title: "찾아 보기",
+  // },
   inquireSubscribedAsset: {
     title: "구독 중인 자산 조회하기",
   },
