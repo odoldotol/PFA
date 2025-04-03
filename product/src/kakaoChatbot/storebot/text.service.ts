67d242ee06628b0b10c3bd2cb7b2385734e0894e
odoldotol <odoldotol@gmail.com>
2025-04-04 00:53:16 +0900

    text

--- product/src/kakaoChatbot/storebot/text.service.ts
+++ product/src/kakaoChatbot/storebot/text.service.ts
@@ -5,10 +5,10 @@ export class StorebotTextService {
 
   public introduction(): string {
     return `간편주문
+
 언제 어디서든
 원하는 매장의 주문을
-카카오톡으로 10초 만에
-내 손안에서 결제까지 안전하게!`;
+내 손안에서 카카오톡으로 10초 만에`;
   }
 
   public scheduleAlarm(): string {
@@ -16,9 +16,7 @@ export class StorebotTextService {
   }
 
   public introductionDetail(): string {
-    return this.noPersonalInfosNoAds() + "\n"
-    + "\n"
-    + `간편주문은 추가로 앱을 설치할 필요도 없고 회원가입할 필요도 없어요.
+    return `간편주문은 추가로 앱을 설치할 필요도 없고 회원가입할 필요도 없어요.
 카운터나 키오스크에 줄 설 필요도 없고 내 핸드폰만 있으면 주문할 수 있어요.
 언제 어디서든 원하는 매장의 주문을 카카오톡으로 10초 만에 하세요.`;
   }
