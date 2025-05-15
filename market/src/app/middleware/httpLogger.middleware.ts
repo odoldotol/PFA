624683897d408e3f4e817c807556121130f5518f
odoldotol <odoldotol@gmail.com>
2025-05-15 20:09:03 +0900

    refac(HttpLogger): use log method as listner, padding duration and method

--- market/src/app/middleware/httpLogger.middleware.ts
+++ market/src/app/middleware/httpLogger.middleware.ts
@@ -1,33 +1,53 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import { Logger } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NestMiddleware
+} from '@nestjs/common';
+import {
+  Request,
+  Response,
+  NextFunction
+} from 'express';
 
 @Injectable()
-export class HttpLoggerMiddleware implements NestMiddleware {
-
+export class HttpLoggerMiddleware
+  implements NestMiddleware
+{
   private readonly logger = new Logger('HttpLogger');
 
-  use(req: Request, res: Response, next: NextFunction) {
-    const now = Date.now();
-    const { method, originalUrl } = req;
-    let listener: (() => void) | null = () => {
-      listener = null;
+  use(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const reqTime = Date.now();
 
-      const responseTime = Date.now() - now;
-      const { statusCode } = res;
-
-      if (
-        originalUrl === '/health' &&
-        method === 'GET' &&
-        (statusCode === 200 || statusCode === 304)
-      ) {
-        return;
-      }
-
-      this.logger.log(`${statusCode} | ${responseTime}ms | ${method} | ${originalUrl}`);
-    };
-    res.on('finish', listener);
+    res.on('finish', this.log.bind(this, reqTime, req, res));
+    
     next();
+  }
+
+  private log(
+    reqTime: number,
+    req: Request,
+    res: Response,
+  ): void {
+    const
+    { method, originalUrl } = req,
+    { statusCode } = res;
+
+    if (
+      originalUrl === '/health' &&
+      method === 'GET' &&
+      (statusCode === 200 || statusCode === 304)
+    ) {
+      return;
+    }
+
+    const responseTime = Date.now();
+    const duration = responseTime - reqTime;
+
+    this.logger.log(`${statusCode} | ${(duration + 'ms').padStart(7)} | ${method.padStart(7)} | ${originalUrl}`);
   }
 
 }
