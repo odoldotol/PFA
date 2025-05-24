bda9152ad1fd95f1ce5c8bd008c26ebdb578f90e
odoldotol <odoldotol@gmail.com>
2025-05-24 19:16:54 +0900

    refac(TextService): no need json text file on dev. and no need to implement its methods but just define.

--- product/src/kakaoChatbot/class/text.service.ts
+++ product/src/kakaoChatbot/class/text.service.ts
@@ -1,8 +1,10 @@
 import { readFileSync } from "fs";
+import { AppConfigService } from "src/config";
 import * as F from "@fxts/core";
 
 /**
- * 주의 - 프로토타입 체인상 직전 프로토타입을 기반으로 json 파일의 유효성을 검사함.
+ * 주의 - 프로토타입 체인상 직전 프로토타입을 기반으로 json 파일의 유효성을 검사함.  
+ * => 자식 클래스에서 매서드를 정의만 하는 것으로 text 디자인이 끝나고 여기에서 디자인을 바탕으로 json 파일 검사 및 메서드를 구현함.
  */
 export abstract class TextService<T extends Record<string, any>> {
 
@@ -11,22 +13,31 @@ export abstract class TextService<T extends Record<string, any>> {
     "data" //
   ];
 
-  protected readonly data: {
-    [key in keyof T]: string
-  }
+  protected readonly data: Data<T>;
 
-  constructor(jsonPath: string) {
-    this.data = this.loadText(jsonPath);
+  /**
+   * production 환경이 아니면 jsonPath 를 읽지 않고 sample 을 만듦.
+   */
+  constructor(
+    jsonPath: string,
+    appConfigSrv: AppConfigService,
+  ) {
+    if (appConfigSrv.isProduction()) {
+      this.data = this.loadText(jsonPath);
+    } else {
+      this.data = this.sampleText();
+    }
 
     if (this.validate() == false) {
       throw new Error(`${this.constructor.name}: Failed to load all text`);
     }
+
+    this.implMethods();
   }
 
-  private validate() :boolean {
+  private validate(): boolean {
     return F.pipe(
-      Object.getOwnPropertyNames(Object.getPrototypeOf(this)),
-      F.difference(this.exception),
+      this.designedKeyIter(),
       F.every(key => typeof this.data[key] == "string")
     );
   }
@@ -35,4 +46,38 @@ export abstract class TextService<T extends Record<string, any>> {
     return JSON.parse(readFileSync(jsonPath, "utf-8"));
   }
 
+  private sampleText(): Data<T> {
+    return F.pipe(
+      this.designedKeyIter(),
+      F.map(key => [key, key] as [keyof T, string]),
+      F.fromEntries,
+    ) as Data<T>;
+  }
+
+  private implMethods() {
+    F.pipe(
+      this.designedKeyIter(),
+      F.each(key => {
+        Object.getPrototypeOf(this)[key] = function () {
+          return this.data[key];
+        };
+
+        Object.defineProperty(Object.getPrototypeOf(this)[key], "name", {
+          value: key,
+        });
+      })
+    );
+  }
+
+  private designedKeyIter(): IterableIterator<keyof T & string> {
+    return F.pipe(
+      Object.getOwnPropertyNames(Object.getPrototypeOf(this)),
+      F.difference(this.exception),
+    );
+  }
+
 }
+
+type Data<T> = {
+  [key in keyof T]: string
+};
--- product/src/kakaoChatbot/storebot/text.service.ts
+++ product/src/kakaoChatbot/storebot/text.service.ts
@@ -1,26 +1,28 @@
+// @ts-nocheck // 메서드를 정의만 하기.
+
 import { Injectable } from "@nestjs/common";
+import { AppConfigService } from "src/config";
 import { TextService } from "../class";
 
 @Injectable()
 export class StorebotTextService
   extends TextService<StorebotTextService>
 {
-  constructor() {
-    super("src/../storebot.text.json");
+  constructor(
+    appConfigSrv: AppConfigService,
+  ) {
+    super(
+      "src/../storebot.text.json",
+      appConfigSrv,
+    );
   }
 
-  public introduction(): string { return this.data.introduction; }
-
-  public scheduleAlarm(): string { return this.data.scheduleAlarm; }
-
-  public introductionDetail(): string { return this.data.introductionDetail; }
-
-  public scheduleAlarmButtonLabel(): string { return this.data.scheduleAlarmButtonLabel; }
-
-  public launchAlarmScheduled(): string { return this.data.launchAlarmScheduled; }
-
-  public needToAddFriend(): string { return this.data.needToAddFriend; }
-
-  public retryScheduleLaunchAlarmAfterAddFriend(): string { return this.data.retryScheduleLaunchAlarmAfterAddFriend; }
+  public introduction(): string {}
+  public scheduleAlarm(): string {}
+  public introductionDetail(): string {}
+  public scheduleAlarmButtonLabel(): string {}
+  public launchAlarmScheduled(): string {}
+  public needToAddFriend(): string {}
+  public retryScheduleLaunchAlarmAfterAddFriend(): string {}
 
 }
